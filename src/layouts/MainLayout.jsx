269cacd2eafa44dbd4759539c066e80873e48b03
AskatAsh <abdulaskat@gmail.com>
2024-12-10 21:43:19 +0600

    added navbar using daisyui and import it to main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/common/Navbar";
+import Footer from './../components/common/Footer';
 
 const MainLayout = () => {
     return (
         <div>
+            <header>
+                <Navbar />
+            </header>
             <main>
                 <Outlet />
             </main>
+
+            {/* Footer Section */}
+            <Footer />
         </div>
     );
 };
