d99162febdb0c250aa3169ad1b15c5c1797189ad
AskatAsh <abdulaskat@gmail.com>
2024-12-14 15:55:39 +0600

    installed axios and get jobs data using get method and useEffect with async and await

--- src/components/FeaturedJobs.jsx
+++ src/components/FeaturedJobs.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import SectionTitles from "./common/SectionTitles";
+import axios from "axios";
 
 const FeaturedJobs = () => {
+    useEffect(() => {
+        getFeaturedJobsData();
+    }, [])
+    const getFeaturedJobsData = async () => {
+        const {data} = await axios.get('http://localhost:5000/jobs')
+        console.log(data);
+    }
     return (
         <section>
             <SectionTitles title="Featured Jobs" subtitle="Know your worth and find the job that qualify your life" />
