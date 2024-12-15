a3c20903596ec5756f2dc4f82fb8262cdf63f063
AskatAsh <abdulaskat@gmail.com>
2024-12-16 03:11:01 +0600

    installed react sweetalert2 and use try and catch method to post job application to server

--- src/components/forms/JobApplyForm.jsx
+++ src/components/forms/JobApplyForm.jsx
@@ -1,35 +1,71 @@
 import Button from "./../common/Button";
-import useAuthContext from './../../hooks/useAuthContext';
+import useAuthContext from "./../../hooks/useAuthContext";
 import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+import axios from "axios";
 
 const JobApplyForm = () => {
-    const {user} = useAuthContext();
-    const {id} = useParams();
-    console.log(id);
+  const { user } = useAuthContext();
+  const { id } = useParams();
 
-    const handleJobApplication = (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const github_url = form.github_url.value;
-        const linkedin_url = form.linkedin_url.value;
-        const resume_url = form.resume_url.value;
-        const jobApplication = {
-            github_url,
-            linkedin_url,
-            resume_url,
-            job_id: id,
-            applicant_email: user?.email,
+  const handleJobApplication = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const github_url = form.github_url.value;
+    const linkedin_url = form.linkedin_url.value;
+    const resume_url = form.resume_url.value;
+    const jobApplication = {
+      github_url,
+      linkedin_url,
+      resume_url,
+      job_id: id,
+      applicant_email: user?.email,
+    };
+    // console.log(jobApplication);
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/job-applications",
+        jobApplication,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-        console.log(jobApplication);
+      );
+
+      if (response.status === 200 || response.status === 201) {
+        // succes message
+        Swal.fire({
+          icon: "success",
+          title: "Application Submitted",
+          text: "Your application has been successfully submitted!",
+        });
+      }
+    } catch (error) {
+      console.log("Error submitting application: ", error);
+      // failure message
+      Swal.fire({
+        icon: "error",
+        title: "Submission Failed",
+        text:
+          error.response?.data?.message ||
+          "An error occurred. Please try again later.",
+      });
     }
+  };
 
   return (
     <section className="pt-10">
       <h1 className="text-3xl font-semibold text-center text-gray-800 dark:text-gray-200">
         Apply Now!
       </h1>
-      <p className="text-center text-gray-600 dark:text-gray-400 mt-4 mb-8">Please make sure to fill up the required fields before submit.</p>
-      <form onSubmit={handleJobApplication} className="max-w-xl mx-auto p-5 md:p-8 rounded-2xl bg-gray-50 dark:bg-gray-950 dark:bg-opacity-80 space-y-6">
+      <p className="text-center text-gray-600 dark:text-gray-400 mt-4 mb-8">
+        Please make sure to fill up the required fields before submit.
+      </p>
+      <form
+        onSubmit={handleJobApplication}
+        className="max-w-xl mx-auto p-5 md:p-8 rounded-2xl bg-gray-50 dark:bg-gray-950 dark:bg-opacity-80 space-y-6"
+      >
         <label className="form-control w-full">
           <div className="label">
             <span className="label-text">Github URL*</span>
