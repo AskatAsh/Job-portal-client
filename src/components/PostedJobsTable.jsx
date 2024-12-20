516b9d315cd783f4ab0cab9a7561cb1bb566ae43
AskatAsh <abdulaskat@gmail.com>
2024-12-20 19:03:09 +0600

    added postedJobsTable component with axios get operations to get posted jobs using email query

---
+++ src/components/PostedJobsTable.jsx
@@ -0,0 +1,89 @@
+import { format } from "date-fns";
+import { FaRegEye } from "react-icons/fa";
+import { GrLocation } from "react-icons/gr";
+import { IoBriefcaseOutline } from "react-icons/io5";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { PropTypes } from 'prop-types';
+
+const PostedJobsTable = ({postedJobs}) => {
+  return (
+    <section className="max-w-7xl mx-auto w-11/12 p-4 md:p-6 lg:p-8 my-16 md:my-20 bg-white dark:bg-gray-900 dark:bg-opacity-50 rounded-xl shadow-md overflow-hidden">
+      {/* Posted jobs table */}
+      <div className="overflow-x-auto mt-8">
+        <table className="table">
+          {/* head */}
+          <thead className="bg-blue-50 dark:bg-blue-950 text-blue-500 text-base">
+            <tr>
+              <th></th>
+              <th className="font-medium py-5">Job Title</th>
+              <th className="font-medium py-5">Deadline</th>
+              <th className="font-medium py-5">Status</th>
+              <th className="font-medium py-5">Action</th>
+            </tr>
+          </thead>
+          <tbody className="text-base text-gray-500 dark:text-gray-400">
+            {/* mapping applied jobs in rows */}
+            {postedJobs.map((postedJob, idx) => (
+              <tr
+                key={postedJob?.job_id}
+                className="hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:bg-opacity-30"
+              >
+                <td className="py-6">{idx + 1}</td>
+                <td className="flex flex-col sm:flex-row gap-4 py-6">
+                  <img
+                    className="w-14 h-14 bg-gray-100 dark:bg-gray-800 rounded-md"
+                    src={postedJob?.company_logo}
+                    alt={`image of ${postedJob?.title}`}
+                  />
+                  <div>
+                    <h2 className="text-gray-800 dark:text-gray-300 font-medium text-lg pb-2">
+                      {postedJob?.title}
+                    </h2>
+                    <ul className="flex items-center gap-3 text-sm flex-wrap">
+                      <li className="flex items-center gap-2 flex-shrink-0">
+                        <IoBriefcaseOutline />
+                        {postedJob?.company}
+                      </li>
+                      <li className="flex items-center gap-2 flex-shrink-0">
+                        <GrLocation />
+                        {postedJob?.location}
+                      </li>
+                    </ul>
+                  </div>
+                </td>
+                <td className="py-6 text-sm">
+                  {format(new Date(postedJob?.deadline), "do LLL, yyyy")}
+                </td>
+                <td className="py-6 text-green-600">
+                  {postedJob?.status}
+                </td>
+                <td className="py-6">
+                  <div className="flex items-start gap-2">
+                    <button
+                      className="w-8 h-8 bg-blue-50 dark:bg-gray-800 rounded-md flex items-center justify-center text-blue-500 hover:bg-blue-400 dark:hover:bg-blue-800 hover:text-white tooltip tooltip-top"
+                      data-tip="view"
+                    >
+                      <FaRegEye />
+                    </button>
+                    <button
+                      className="w-8 h-8 bg-red-50 dark:bg-gray-800 rounded-md flex items-center justify-center text-red-500 hover:bg-red-400 dark:hover:bg-red-800 hover:text-white tooltip tooltip-top"
+                      data-tip="Delete"
+                    >
+                      <RiDeleteBin6Line />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
+
+PostedJobsTable.propTypes = {
+    postedJobs: PropTypes.array
+}
+
+export default PostedJobsTable;
--- src/pages/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs.jsx
@@ -1,10 +1,66 @@
+import { useCallback, useEffect, useState } from "react";
+import PostedJobsTable from "../components/PostedJobsTable";
+import useAuthContext from "./../hooks/useAuthContext";
+import axios from "axios";
+import Loading from "../components/common/Loading";
+import ErrorMessages from "../components/common/ErrorMessages";
 
 const MyPostedJobs = () => {
-    return (
-        <div className="pt-10 sm:pt-16">
-            <h1 className="text-3xl font-semibold text-center px-4 text-gray-800 dark:text-gray-300 mt-16">My Posted Jobs</h1>
-        </div>
-    );
+  const { user } = useAuthContext();
+  const [postedJobs, setPostedJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState({});
+
+  const getMyPostedJobs = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      // get applied jobs data
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_SERVER}/jobs?email=${user?.email}`
+      );
+
+      setPostedJobs(data);
+    } catch (error) {
+      // handle errors
+      console.log(error);
+      const errorMessage = {
+        error: error.message,
+        status: error.response?.status || "N/A",
+        message:
+          error.response?.data?.message ||
+          (error.request
+            ? "Bad Request: Failed to fetch Posted Jobs."
+            : "An unexpected error occurred. Please try again."),
+        code: error.code || "N/A",
+      };
+      setErrorMessage(errorMessage);
+    } finally {
+      // close the loader
+      setIsLoading(false);
+    }
+  }, [user?.email]);
+
+  useEffect(() => {
+    getMyPostedJobs();
+  }, [getMyPostedJobs]);
+
+  return (
+    <div className="pt-10 sm:pt-16">
+      <h1 className="text-3xl font-semibold text-center px-4 text-gray-800 dark:text-gray-300 mt-16">
+        My Posted Jobs
+      </h1>
+      {isLoading ? (
+        <Loading />
+      ) : errorMessage.message ? (
+        <ErrorMessages
+          errorMessage={errorMessage}
+          onRetry={() => getMyPostedJobs()}
+        />
+      ) : (
+        <PostedJobsTable postedJobs={postedJobs} />
+      )}
+    </div>
+  );
 };
 
 export default MyPostedJobs;
