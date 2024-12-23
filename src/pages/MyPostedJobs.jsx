04776f2ab42f3ffabd408d507c2acf6f4e71cc1f
AskatAsh <abdulaskat@gmail.com>
2024-12-23 22:28:21 +0600

    showed message and addjob route button link when no job post added

--- src/pages/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs.jsx
@@ -4,6 +4,8 @@ import useAuthContext from "./../hooks/useAuthContext";
 import axios from "axios";
 import Loading from "../components/common/Loading";
 import ErrorMessages from "../components/common/ErrorMessages";
+import { Link } from "react-router-dom";
+import { ROUTES } from "./../shared/constants/routes";
 
 const MyPostedJobs = () => {
   const { user } = useAuthContext();
@@ -18,7 +20,8 @@ const MyPostedJobs = () => {
       const { data } = await axios.get(
         `${import.meta.env.VITE_SERVER}/jobs?email=${user?.email}`
       );
-    //   console.log(data);
+
+      // console.log(data);
       setPostedJobs(data);
     } catch (error) {
       // handle errors
@@ -56,8 +59,20 @@ const MyPostedJobs = () => {
           errorMessage={errorMessage}
           onRetry={() => getMyPostedJobs()}
         />
+      ) : postedJobs.length ? (
+        <>
+          <PostedJobsTable postedJobs={postedJobs} />
+        </>
       ) : (
-        <PostedJobsTable postedJobs={postedJobs} />
+        <div className="max-w-3xl flex flex-col items-center mx-auto my-16 gap-5">
+          <h3>Please Add a Job Post and come again...</h3>
+          <Link
+            to={ROUTES.ADDJOB}
+            className="btn font-medium bg-blue-600 hover:bg-blue-700 text-white"
+          >
+            Go to Add Job
+          </Link>
+        </div>
       )}
     </div>
   );
