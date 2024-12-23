8b1ab00ded457d0ee58ba7f0ffd676eb2bd67c6b
AskatAsh <abdulaskat@gmail.com>
2024-12-23 21:04:46 +0600

    added axios interceptor to axiosInstance and navigated to login page for forbidden access

--- src/hooks/useAxiosSecure.js
+++ src/hooks/useAxiosSecure.js
@@ -1,12 +1,45 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuthContext from "./useAuthContext";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "./../shared/constants/routes";
+import toast from "react-hot-toast";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_SERVER,
-    withCredentials: true,
-})
+  baseURL: import.meta.env.VITE_SERVER,
+  withCredentials: true,
+});
 
 const useAxiosSecure = () => {
-    return axiosInstance;
+  const { signOutUser, setUser } = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axiosInstance.interceptors.response.use(
+      // interceptor response
+      (response) => {
+        return response;
+      },
+      (error) => {
+        // interceptor error
+        if (error.status === 401 || error.status === 403) {
+        //   console.log(error);
+          signOutUser()
+            .then(() => {
+              toast.success("Forbidden Access! Signed out!");
+              setUser(null);
+              // navigate to login page
+              navigate(ROUTES.TOLOGIN);
+            })
+            .catch((error) => {
+              console.log("Sign out error: ", error);
+            });
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [navigate, setUser, signOutUser]);
+  return axiosInstance;
 };
 
 export default useAxiosSecure;
